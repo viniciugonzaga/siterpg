12b8d2895c77e3b3bb25f7a5d5722dfd98997345
Vinícius Gonzaga Pereira <166918377+viniciugonzaga@users.noreply.github.com>
2024-12-13 11:44:33 -0300

    13/12 casa

--- js/dinos.js
+++ js/dinos.js
@@ -83,15 +83,118 @@ document.addEventListener("scroll", () => {
         footer.style.background = "linear-gradient(45deg, #0f0c29, #302b63, #24243e)"; // Mantém a cor padrão
     }
 });
-const correctPassword = ","; // Altere para a senha desejada
+
 
 //dinos
 
 const creatures = {
-    'Carnívoros': ['T-Rex', 'Velociraptor', 'Spinosaurus'],
-    'Herbívoros': ['Triceratops', 'Stegosaurus', 'Brachiosaurus'],
-    'Onívoros': ['Gallimimus', 'Oviraptor'],
-    'Apex Criaturas': ['Giganotosaurus', 'Indominus Rex']
+    'Carnívoros': [
+    'T-Rex',
+    'Dilofossauro',
+    'Compsognathus',
+    'Celófode',
+    'Proceratossauro',
+    'Trodonte',
+    'Oviraptor',
+    'Hiena',
+    'Cachorro-da-Caveira',
+    'Ave-do-Terror',
+    'Austroraptor',
+    'Moros-Intrepidus',
+    'Sinopterix',
+    'Criolofossauro',
+    'Pinguim',
+    'arqueopterix',
+    'Piranha',
+    'Parasitas-grudante',
+    'parasitas-de-sangue',
+    'parasitas-de-carne',
+    'Tubarão-perseguidor',
+    'Tubarão-tigre',
+    'Tubarão-fantasma',
+    'Tubarão-vulcânico',
+    'Tubarão-cobra',
+    'dunkleosteus',
+    'Megalodon',
+    'Orca',
+    'Tubarão-do-diabo',
+    'plesiosauros',
+    'liopleurodonte',
+    'helicoprion',
+    'poraque',
+    'peixe-pescador',
+    'Carnotauro',
+    'Majugassauro',
+    'Abelissauros',
+    'Carcharodontossauro',
+    'Acrocantossauros',
+    'Giganotossauros',
+    'Alossauros',
+    'Ceratossauro',
+    'Deinonico',
+    'Albertossauro',
+    'Hererassauros',
+    'Metriacontossauro',
+    'Rajassauros',
+    'Yutiranos',
+    'Suchumimus',
+    'Barionix',
+    'Irritator',
+    'Oshalaia',
+    'Dimotrodonte',
+    'Deynocheirus',
+    'Espinossauro',
+    'Crocodilo',
+    'Sarco-imperador',
+    'Purussauros',
+    'Deinosuchus',
+    'Leão marsupial',
+    'Purlovia',
+    'Dente de sabre',
+    'Lobo-terrível',
+    'Andreorsachus',
+    'Velociraptor',
+    'Antrorraptor',
+    'Pyroraptor',
+    'Ptero',
+    'Quetzal',
+    'Tapejara',
+    'Tropeugnathos',
+    'Dimorfonte',
+    'Desmodus-draculai',
+    'Orc-morcego',
+    'Argentavis',
+    'Pelagornis',
+    'Rhynoghtinata',
+    'Aranha-lobo',
+    'Aranha-armadeira',
+    'Aranha-marrom',
+    'Aranha-da-areia',
+    'Aranha-viúva-negra',
+    'Antropleura',
+    'Lagosta',
+    'Lagosta-boxeadora',
+    'Siri',
+    'Camarão',
+    'Rola bosta',
+    'Formiga',
+    'Lesma',
+    'Megalossauro',
+    'Meraxes',
+    'Antrodemus'
+    ],
+        
+
+
+
+
+
+    'Herbívoros': 
+    ['Triceratops', 'Stegosaurus', 'Brachiosaurus'],
+    'Onívoros': 
+    ['Gallimimus', 'Oviraptor'],
+    'Apex Criaturas': 
+    ['Indominus Rex']
 };
 
 const dinoFichas = {
@@ -99,13 +202,19 @@ const dinoFichas = {
         title: 'Rei da Ilha',
         image: 'imagens/dinos_T-rex_ficha.jpg', // Caminho válido
         weight: '8 toneladas',
-        height: '6 metros',
-        length: '12 metros',
-        attributes: { agi: 3, for: 5, int: 2, pre: 2 , vig:4 },
+        height: '7 M',
+        length: '13,5 M',
+        attributes: { agi: 3, for: 5, int: 2, pre: 2, vig: 4 },
+        life: 360, // Vida do dinossauro
+        armor: 25, // Armadura do dinossauro
+        actionBonus: '+25 mordida, +20 intimidar, +20 cheirar, +20 pisar, +20 imobilizar, +15 cheirar, +10 cabeçada, +20 contra-ataque, +10 esquiva, +10 bloquear +5 corrida ', // Bônus de ação de criatura
         attacks: ['Mordida Poderosa: 16d10+30', 'Investida: 7d10+10', 'Ataque de cauda: 7d10+20'],
-        abilities: ['Tirano: O tiranossauro rex é uma criatura implacável que consome todos, no começo da luta o tiranossauro aplica um grito de lentidão. Sua tirania é implacável, durante a luta quando está machucado o tiranossauro ganha +2d6 de acerto e deixa o inimigo fraco.', 'Coragem do rei: Não sofre efeito de medo'],
-        passiva: 'Domínio Territorial',
-        passivaElemental: 'Fúria Ígnea'
+        abilities: [
+            'Tirano: O tiranossauro rex é uma criatura implacável que consome todos, no começo da luta o tiranossauro aplica um grito de lentidão de 1d6 na corrida. Sua tirania é implacável, durante a luta quando está machucado o tiranossauro ganha um buff na mordida que causa o efeito quebrado e se um dado for extremo ganha mais 1 para extremo.',
+            'Coragem do rei: Não sofre efeito de medo e efeitos negativos de qualquer intimidação.'
+        ],
+        passiva: 'Mordida destruidora: A mordida do tiranossauro rex é tão forte que pode causar efeitos de quebrado, sangramento ou dilacerado para toda mordida.',
+        passivaElemental: 'FOGO: O tiranossauro pode fazer seu corpo pegar fogo nas investidas e rabadas, causando dano ao contato de 3d12 de fogo, por rodada 1d12+2d6 (1d6 rod). MORTE: Causa um dano extra a inimigos com pouca vida na mordida +20, 1/4 do dano da mordida faz ele devorar a carne e regenerar vida. AETHER: O tiranossauro ganha um buff de vida e gereneração por quantos inimigos estiver enfretando 1 = 30 de regeneração'
     },
     // Adicione outras fichas aqui
 };
@@ -143,6 +252,11 @@ function openDinoFicha(dino) {
     document.getElementById('dinoInt').textContent = ficha.attributes.int;
     document.getElementById('dinoPre').textContent = ficha.attributes.pre;
     document.getElementById('dinoVig').textContent = ficha.attributes.vig;
+
+    document.getElementById('dinoLife').textContent = ficha.life;
+    document.getElementById('dinoArmor').textContent = ficha.armor;
+    document.getElementById('dinoActionBonus').textContent = ficha.actionBonus;
+
     document.getElementById('dinoAtk1').textContent = ficha.attacks[0];
     document.getElementById('dinoAtk2').textContent = ficha.attacks[1];
     document.getElementById('dinoAtk3').textContent = ficha.attacks[2];
@@ -154,7 +268,6 @@ function openDinoFicha(dino) {
     // Exibir modal
     document.getElementById('dinoModal').style.display = 'flex';
 }
-
 function closeDinoModal() {
     document.getElementById('dinoModal').style.display = 'none';
 }
